bded591fa5ceabe06806a5686e61e3ea8825d4e5
Sean Voeller <sean.voeller@gmail.com>
2019-08-09 19:23:36 -0700

    refactor

--- Persistence/Contexts/AppDbContext.cs
+++ Persistence/Contexts/AppDbContext.cs
@@ -2,13 +2,14 @@ using System;
 using HelloAspNet.Domain.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.InMemory.ValueGeneration.Internal;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace HelloAspNet.Persistence.Contexts
 {
     public class AppDbContext : DbContext
     {
         public DbSet<Category> Categories { get; set; }
-        public DbSet<Product> Products { get; set;}
+        public DbSet<Product> Products { get; set; }
 
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { } // DUDE! what is this extends syntax at the method level?
 
@@ -16,26 +17,25 @@ namespace HelloAspNet.Persistence.Contexts
         {
             base.OnModelCreating(builder);
 
-            // TODO: can probably chain these calls
+            EntityTypeBuilder<Category> categoryTypeBuilder = builder.Entity<Category>();
+            categoryTypeBuilder.ToTable("Categories").HasKey(p => p.Id);
+            categoryTypeBuilder.Property(p => p.Id).IsRequired().ValueGeneratedOnAdd().HasValueGenerator<InMemoryIntegerValueGenerator<int>>();
+            categoryTypeBuilder.Property(p => p.Name).IsRequired().HasMaxLength(50);
+            categoryTypeBuilder.HasMany(p => p.Products).WithOne(p => p.Category).HasForeignKey(p => p.CategoryId);
 
-            builder.Entity<Category>().ToTable("Categories");
-            builder.Entity<Category>().HasKey(p => p.Id);
-            builder.Entity<Category>().Property(p => p.Id).IsRequired().ValueGeneratedOnAdd().HasValueGenerator<InMemoryIntegerValueGenerator<int>>();
-            builder.Entity<Category>().Property(p => p.Name).IsRequired().HasMaxLength(50);
-            builder.Entity<Category>().HasMany(p => p.Products).WithOne(p => p.Category).HasForeignKey(p => p.CategoryId);
-
-            builder.Entity<Category>().HasData
+            categoryTypeBuilder.HasData
             (
               new Category { Id = 100, Name = "Fruits and Vegetables" },
               new Category { Id = 101, Name = "Dairy" }
             );
 
-            builder.Entity<Product>().ToTable("Products");
-            builder.Entity<Product>().HasKey(p => p.Id);
-            builder.Entity<Product>().Property(p => p.Id).IsRequired().ValueGeneratedOnAdd();
-            builder.Entity<Product>().Property(p => p.Name).IsRequired().HasMaxLength(50);
-            builder.Entity<Product>().Property(p => p.QuantityInPackage).IsRequired();
-            builder.Entity<Product>().Property(p => p.UnitOfMeasurement).IsRequired();
+            EntityTypeBuilder<Product> productTypeBuilder = builder.Entity<Product>();
+            productTypeBuilder.ToTable("Products");
+            productTypeBuilder.HasKey(p => p.Id);
+            productTypeBuilder.Property(p => p.Id).IsRequired().ValueGeneratedOnAdd();
+            productTypeBuilder.Property(p => p.Name).IsRequired().HasMaxLength(50);
+            productTypeBuilder.Property(p => p.QuantityInPackage).IsRequired();
+            productTypeBuilder.Property(p => p.UnitOfMeasurement).IsRequired();
         }
     }
 }
